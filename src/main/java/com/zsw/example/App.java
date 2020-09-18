3bf35a32f786e1fe354ab06352d4d68afb5d2c34
zhangshiwei <zhangshiwei199204@163.com>
2020-09-18 13:56:33 +0800

    2

--- src/main/java/com/zsw/example/App.java
+++ src/main/java/com/zsw/example/App.java
@@ -5,6 +5,6 @@ package com.zsw.example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
     }
 }
